071a33ff0f4b1f803cd650d833296d9ae1728e85
Ritesh Patel <ritesh.patel@pragmaticplay.com>
2024-05-06 20:44:41 +0530

    Display view according to turn (added view perspective)

--- go/cliches.go
+++ go/cliches.go
@@ -15,44 +15,75 @@ const RESET = ""
 /* Declare Chess Board */
 type ChessBoard [8][8]string
 var chessBoard ChessBoard = ChessBoard{
-  {"w5", "w3", "w4", "w9", "w8", "w4", "w3", "w5"},
+  {"w5", "w3", "w4", "w8", "w9", "w4", "w3", "w5"},
   {"w1", "w1", "w1", "w1", "w1", "w1", "w1", "w1"},
   {"00", "00", "00", "00", "00", "00", "00", "00"},
   {"00", "00", "00", "00", "00", "00", "00", "00"},
   {"00", "00", "00", "00", "00", "00", "00", "00"},
   {"00", "00", "00", "00", "00", "00", "00", "00"},
   {"b1", "b1", "b1", "b1", "b1", "b1", "b1", "b1"},
-  {"b5", "b3", "b4", "b9", "b8", "b4", "b3", "b5"},
+  {"b5", "b3", "b4", "b8", "b9", "b4", "b3", "b5"},
 }
 
 
 /* Function to display the chess board */
 func displayBoard(cb ChessBoard, perspective string) {
-  /* Raw chess board array */
   fmt.Println()
   fmt.Print("   ")
   for i := 0; i < 8; i++ {
     fmt.Print("---- ")
   }
   fmt.Println()
-  for i := 0; i < 8; i++ {
-    fmt.Print("  | ")
-    for j := 0; j < 8; j++ {
-      fmt.Print(cb[i][j], " | ")
+
+  /* Raw chess board array */
+  if (perspective == "computer") {
+    for i := 0; i < 8; i++ {
+      fmt.Print("  | ")
+      for j := 0; j < 8; j++ {
+        fmt.Print(cb[i][j], " | ")
+      }
+      fmt.Println()
+      fmt.Print("   ")
+      for j := 0; j < 8; j++ {
+        fmt.Print("---- ")
+      }
+      fmt.Println()
     }
-    fmt.Println()
-    fmt.Print("   ")
-    for j := 0; j < 8; j++ {
-      fmt.Print("---- ")
-    }
-    fmt.Println()
   }
-  fmt.Println()
 
   /* If perspective is white */
+  if (perspective == "white") {
+    for i := 7; i >= 0; i-- {
+      fmt.Print("  | ")
+      for j := 0; j < 8; j++ {
+        fmt.Print(cb[i][j], " | ")
+      }
+      fmt.Println()
+      fmt.Print("   ")
+      for j := 0; j < 8; j++ {
+        fmt.Print("---- ")
+      }
+      fmt.Println()
+    }
+  }
 
+  /* If perspective is black */
+  if (perspective == "black") {
+    for i := 0; i < 8; i++ {
+      fmt.Print("  | ")
+      for j := 7; j >= 0; j-- {
+        fmt.Print(cb[i][j], " | ")
+      }
+      fmt.Println()
+      fmt.Print("   ")
+      for j := 0; j < 8; j++ {
+        fmt.Print("---- ")
+      }
+      fmt.Println()
+    }
+  }
 
-  /*If perspective is black */
+  fmt.Println()
 }
 
 
@@ -69,7 +100,6 @@ func isValidMove(cb ChessBoard, move string) bool {
   var destPositionX int
   var destPositionY int
   if l >= 5 {
-
   }
 
   fmt.Println(piece, currentPositionX, currentPositionY, destPositionX, destPositionY)
