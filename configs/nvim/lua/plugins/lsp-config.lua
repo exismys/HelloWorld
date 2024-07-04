50345a6dc7f3173737ef2641d615100a69d665b2
Ritesh Patel <ritesh.patel@pragmaticplay.com>
2024-07-05 00:16:39 +0530

    Update nvim config: Add terraform ls and gitsigns

--- configs/nvim/lua/plugins/lsp-config.lua
+++ configs/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "gopls" }
+        ensure_installed = { "lua_ls", "gopls", "terraformls" }
       })
     end
   },
@@ -19,6 +19,7 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.gopls.setup({})
+      lspconfig.terraformls.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
