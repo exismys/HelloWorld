d3826e767d47d48f1eb8ab06beb28e8eb8fa1a63
Ritesh Patel <ritesh.patel@pragmaticplay.com>
2024-07-04 23:18:08 +0530

    Update nvim config: Added lsp, telescope dropdown, indent-blankline

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
   return {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-        ensure_installed = {"lua", "javascript", "go", "python", "html", "css"},
+        ensure_installed = {"lua", "javascript", "go", "python", "html", "css", "bash", "terraform", "yaml"},
         sync_install = false,
-        hightlight = {enable = true },
+        highlight = {enable = true },
         indent = { enable = true }
       })
     end
--- configs/nvim/lua/vim-options.lua
+++ configs/nvim/lua/vim-options.lua
@@ -1,7 +1,7 @@
-vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set expandtab")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set clipboard+=unnamedplus")
