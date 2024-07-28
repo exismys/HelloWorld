43c14379b255fd6a2eeffd0b77616df725cbf509
Ritesh Patel <ritesh.patel@pragmaticplay.com>
2024-07-28 13:09:15 +0530

    canvas acc-vel

--- canvas/simulations/acc-vel/acc-val.js
+++ canvas/simulations/acc-vel/acc-val.js
@@ -1,9 +1,48 @@
 const canvas = document.querySelector("canvas")
 
-canvas.width = 1280
-canvas.height = 720
+function setupCanvas() {
+  canvas.width = 1280
+  canvas.height = 720
+  return canvas.getContext("2d")
+}
 
-const ctx = document.getContext("2d")
+const ctx = setupCanvas()
 
-slkjdfkjs = sdlfj
+class Circle {
+  constructor(x, y, r) {
+    this.x = x
+    this.y = y
+    this.r = r
+    this.velocity = 1
+    this.acceleration = 1
+  }
 
+  draw() {
+    ctx.beginPath()
+    ctx.fillStyle = "#000000"
+    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, true)
+    ctx.closePath()
+    ctx.fill()
+  }
+
+  update() {
+    this.velocity += this.acceleration
+    this.x += this.velocity
+  }
+}
+
+let circle
+function init() {
+  circle = new Circle(50, 50, 10)
+}
+
+init()
+
+function animate() {
+  requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  circle.draw()
+  circle.update()
+}
+
+animate()
