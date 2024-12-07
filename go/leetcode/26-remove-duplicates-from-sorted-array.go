d0cd380d99fc970a7385f7a0024de90932320fb6
Ritesh Patel <riteshzeronity@gmail.com>
2024-12-07 22:48:43 +0530

    leetcode #26 - implement O(n) solution

--- go/leetcode/26-remove-duplicates-from-sorted-array.go
+++ go/leetcode/26-remove-duplicates-from-sorted-array.go
@@ -1,6 +1,7 @@
 package main
 
 // time complexity: O(n^2)
+// shift one step each time a duplicate occurs
 // func removeDuplicates(nums []int) int {
 // 	len := len(nums)
 // 	for i := 1; i < len; i++ {
@@ -15,24 +16,38 @@ package main
 // 	return len
 // }
 
-func removeDuplicates(nums []int) int {
-	len := len(nums)
-	for i := 0; i < len-1; i++ {
-		j := i + 1
-		for j < len && nums[j] == nums[i] {
-			j++
-		}
+// time complexity: O(n^2)
+// shift duplicate-1 step
+// func removeDuplicates(nums []int) int {
+// 	len := len(nums)
+// 	for i := 0; i < len-1; i++ {
+// 		j := i + 1
+// 		for j < len && nums[j] == nums[i] {
+// 			j++
+// 		}
 
-		// j-i is the frequency of an unique element
-		// so if j-i > 1, there are duplicates
-		// so we need to remove j-i-1 elements
-		// lenth will be shinked by the same
-		if j-i > 1 {
-			for k := i + 1; k < len-(j-i-1); k++ {
-				nums[k] = nums[k+(j-i-1)]
-			}
-			len -= j - i - 1
+// 		// j-i is the frequency of an unique element
+// 		// so if j-i > 1, there are duplicates
+// 		// so we need to remove j-i-1 elements
+// 		// lenth will be shinked by the same
+// 		if j-i > 1 {
+// 			for k := i + 1; k < len-(j-i-1); k++ {
+// 				nums[k] = nums[k+(j-i-1)]
+// 			}
+// 			len -= j - i - 1
+// 		}
+// 	}
+// 	return len
+// }
+
+// time complexity: O(n)
+func removeDuplicates(nums []int) int {
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
 		}
 	}
-	return len
+	return i + 1
 }
