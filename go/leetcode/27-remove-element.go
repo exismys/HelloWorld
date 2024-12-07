cb277b182140731cc9f6f4f4a7f5c1ea31d76323
Ritesh Patel <riteshzeronity@gmail.com>
2024-12-07 12:49:23 +0530

    leetcode #27 - remove element

---
+++ go/leetcode/27-remove-element.go
@@ -0,0 +1,16 @@
+package main
+
+// time complexity: O(n^2)
+func removeElement(nums []int, val int) int {
+	len := len(nums)
+	for i := 0; i < len; i++ {
+		if nums[i] == val {
+			for j := i; j < len-1; j++ {
+				nums[j] = nums[j+1]
+			}
+			i--
+			len--
+		}
+	}
+	return len
+}
--- go/leetcode/main.go
+++ go/leetcode/main.go
@@ -5,13 +5,13 @@ import (
 )
 
 func main() {
-	// 1 - Two Sum
+	// 1 - two Sum
 	// nums := []int{2, 7, 11, 15}
 	// target := 9
 	// indices := twoSum(nums, target)
 	// fmt.Printf("[%d %d]", indices[0], indices[1])
 
-	// 2 - Add two numbers
+	// 2 - add two numbers
 	// ln1 := &ListNode{
 	//   val: 2,
 	//   Next: &ListNode{
@@ -33,14 +33,17 @@ func main() {
 	//     },
 	//   },
 	// }
-
 	// ln3 := addTwoNumbers(ln1, ln2)
-
 	// for ln3 != nil {
 	//   fmt.Printf("%d", ln3.val)
 	//   ln3 = ln3.Next
 	// }
 
-	// 3 - Longest substring
-	fmt.Println(lenLongestSubstring("aaa"))
+	// 3 - longest substring
+	// fmt.Println(lenLongestSubstring("aaa"))
+
+	// 27 - remove element
+	nums := []int{5, 4, 10, 11, 9, 24, 6, 10, 10, 4}
+	fmt.Println(removeElement(nums, 10))
+	fmt.Println(nums)
 }
