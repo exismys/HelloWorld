238423a44586412e515f43ad29149867f98a8ac4
Ritesh Patel <ritesh.patel@arrise.com>
2025-07-24 12:47:53 +0530

    alternative udp comment

--- python/networking/udp_client.py
+++ python/networking/udp_client.py
@@ -5,6 +5,11 @@ server_port = 12000
 
 client_socket = socket(AF_INET, SOCK_DGRAM)
 
+# We can additionally bind this socket with a port number
+# which is otherwise handled by the OS internally
+
+# client_socket.bind(('', 19157))
+
 message = input("Enter lowercase sentence: ")
 
 client_socket.sendto(message.encode(), (server_name, server_port))
