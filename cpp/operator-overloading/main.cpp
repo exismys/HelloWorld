45d20f9482c9bcc48ab81cbafd26d4c0e41f8506
Ritesh Patel <ritesh.patel@arrise.com>
2025-09-08 12:45:35 +0530

    Update structure

--- cpp/operator-overloading/main.cpp
+++ cpp/operator-overloading/main.cpp
@@ -5,6 +5,6 @@ int main() {
     SpecialNumber num1 = 10;
     SpecialNumber num2 = 5;
     SpecialNumber sum = num1 + num2;
-    std::cout << "Sum: " << sum.value << std::endl;
+    std::cout << "Sum: " << sum.value << std::endl; // Output: Sum: 5 (+ have been overloaded to perform subtraction)
     return 0;
 }
